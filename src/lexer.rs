29ccf614eea2cf262d85177b533c74aa000e251b
badatkovaak <122653634+badatkovaak@users.noreply.github.com>
2024-01-29 03:10:10 +0300

    1

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,10 +1,4 @@
-const KWORDS: &[&str] = &[
-    "break", "const", "continue", "do", "else", "enum", "false", "float", "fn", "for", "if",
-    "import", "int", "let", // "match",
-    "return", "struct", "then", "true",
-];
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Token {
     Illegal,
     Break,
@@ -20,15 +14,16 @@ pub enum Token {
     If,
     Int,
     Let,
-    // Match,
     Return,
     Struct,
     Then,
     True,
-    Id(Box<str>),
+    Id(Rc<str>),
     IntLiteral(u64),
     FloatLiteral(f64),
-    StringLiteral(Box<str>),
+    StringLiteral(Rc<str>),
+    Pipe,
+    Composition,
     Assign,
     Equal,
     NotEqual,
@@ -42,13 +37,13 @@ pub enum Token {
     Minus,
     Mult,
     Div,
-    // Caret,
     LParen,
     RParen,
     LSquare,
     RSquare,
     Colon,
     Comma,
+    Dot,
 }
 
 #[derive(Debug)]
@@ -57,15 +52,102 @@ pub enum LexerError {
     LexErr,
 }
 
+use std::{collections::HashMap, rc::Rc};
+
 use LexerError as LE;
 use Token as T;
 
-pub fn tokenize(input: String) -> Result<Vec<Token>, LexerError> {
+// const KWORDS: &[&str] = &[
+//     "break", "const", "continue", "do", "else", "enum", "false", "float", "fn", "for", "if",
+//     "import", "int", "let", // "match",
+//     "return", "struct", "then", "true",
+// ];
+
+pub fn lex(input: &str) -> Result<Vec<Token>, LexerError> {
+    let keywords: HashMap<&str, Token> = HashMap::from([
+        ("break", T::Break),
+        ("const", T::Const),
+        ("continue", T::Continue),
+        ("do", T::Do),
+        ("else", T::Else),
+        ("enum", T::Enum),
+        ("false", T::False),
+        ("float", T::Float),
+        ("fn", T::Func),
+        ("for", T::For),
+        ("if", T::If),
+        ("int", T::Int),
+        ("let", T::Let), // "match",
+        ("return", T::Return),
+        ("struct", T::Struct),
+        ("then", T::Then),
+        ("true", T::True),
+    ]);
+
     let mut toks = vec![];
     let mut chars = input.chars().peekable();
 
     while let Some(&c) = chars.peek() {
+        println!("{:?}", toks);
         match c {
+            a if a.is_ascii_digit() || a == '.' => {
+                chars.next();
+                let mut num = String::from(a);
+                let mut is_float = a == '.';
+
+                while let Some(&d) = chars.peek() {
+                    match d {
+                        a if a.is_ascii_digit() => {
+                            num.push(a);
+                        }
+                        '.' => {
+                            if !is_float {
+                                is_float = true;
+                                num.push('.');
+                            } else {
+                                return Err(LE::IllegalCharacter);
+                            }
+                        }
+                        _ => {
+                            break;
+                        }
+                    }
+                    chars.next();
+                }
+
+                if !is_float {
+                    let x = num.parse::<u64>();
+                    match x {
+                        Ok(a) => {
+                            toks.push(T::IntLiteral(a));
+                        }
+                        Err(_) => {
+                            println!("Error during parsing of Integer Literal");
+                            println!("{}", num);
+                            return Err(LE::LexErr);
+                        }
+                    }
+                } else if num.len() > 1 {
+                    if let Some(s) = num.get(0..1) {
+                        if s == "." {
+                            num.insert(0, '0');
+                        }
+                    }
+                    let x = num.parse::<f64>();
+                    match x {
+                        Ok(a) => {
+                            toks.push(T::FloatLiteral(a));
+                        }
+                        Err(_) => {
+                            println!("Error during parsing of Float Literal");
+                            println!("{}", num);
+                            return Err(LE::LexErr);
+                        }
+                    }
+                } else {
+                    toks.push(T::Dot);
+                }
+            }
             '+' => {
                 toks.push(T::Plus);
                 chars.next();
@@ -84,6 +166,10 @@ pub fn tokenize(input: String) -> Result<Vec<Token>, LexerError> {
             }
             ',' => {
                 toks.push(T::Comma);
+                chars.next();
+            }
+            '.' => {
+                toks.push(T::Dot);
                 chars.next();
             }
             '(' => {
@@ -106,6 +192,36 @@ pub fn tokenize(input: String) -> Result<Vec<Token>, LexerError> {
                 toks.push(T::Colon);
                 chars.next();
             }
+            '&' => {
+                chars.next();
+                if let Some(&d) = chars.peek() {
+                    if d == '&' {
+                        toks.push(T::And);
+                        chars.next();
+                    } else {
+                        return Err(LE::IllegalCharacter);
+                    }
+                }
+            }
+            '|' => {
+                chars.next();
+                if let Some(&d) = chars.peek() {
+                    match d {
+                        '|' => {
+                            toks.push(T::Or);
+                            chars.next();
+                        }
+                        '>' => {
+                            toks.push(T::Pipe);
+                            chars.next();
+                        }
+                        _ => {
+                            return Err(LE::IllegalCharacter);
+                        }
+                    }
+                }
+            }
+
             '<' => {
                 chars.next();
                 if let Some(&d) = chars.peek() {
@@ -126,6 +242,10 @@ pub fn tokenize(input: String) -> Result<Vec<Token>, LexerError> {
                     match d {
                         '=' => {
                             toks.push(T::GreaterTEq);
+                            chars.next();
+                        }
+                        '>' => {
+                            toks.push(T::Composition);
                             chars.next();
                         }
                         _ => {
@@ -159,7 +279,35 @@ pub fn tokenize(input: String) -> Result<Vec<Token>, LexerError> {
                     }
                 }
             }
-            a if a.is_ascii_alphabetic() => {}
+            ' ' => {
+                chars.next();
+            }
+            a if a.is_ascii_alphabetic() || a == '_' => {
+                let mut name = String::from(a);
+
+                chars.next();
+
+                while let Some(&d) = chars.peek() {
+                    match d {
+                        a if a.is_ascii_alphanumeric() || a == '_' => {
+                            name.push(a);
+                            chars.next();
+                        }
+                        _ => {
+                            break;
+                        }
+                    }
+                    if name.len() >= 65 {
+                        return Err(LE::LexErr);
+                    }
+                    // chars.next();
+                }
+                if let Some(x) = keywords.get(name.as_str()) {
+                    toks.push(x.clone());
+                } else {
+                    toks.push(T::Id(Rc::from(name)));
+                }
+            }
             _ => {
                 return Err(LE::LexErr);
             }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,19 @@ pub mod lexer;
 pub mod lexer1;
 pub mod parser;
 
-use lexer1::lex;
+use lexer::lex;
+// use lexer1::lex;
 
 fn main() {
     println!();
-    let toks = lex("-. + 3 - 10^3/2");
+    // let toks1 = lex("-. + 3 - 10^3/2");
+    // println!("{:?}", toks1);
+
+    // let toks = tokenize("10 + 3 - 27 * 2.5 / .6 : < > <= >= == != = , . ( ) [ ] && || |> >> mynaem fn for if do let x 'mystr' ");
+
+    let toks = lex(
+        "10 + 3 - 27 * 2.5 / 0.6 - .7  * 1. : < > <= >= == != = , . ( ) [ ] && || |> >> fn re x",
+    );
     println!("{:?}", toks);
     println!();
 }
